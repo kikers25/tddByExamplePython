66bdc66f5d17e2f4810683293bd13c2fe8318d83
Martin Enrique, ESD <Enrique.Martin@audatexdatos.es>
2016-05-22 23:01:31 +0200

    first commit

---
+++ ch21_2.py
@@ -0,0 +1,53 @@
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+	def setUp(self):
+		pass
+	def run(self):
+		result = TestResult()
+		result.testStarted()
+		self.setUp()
+		method = getattr(self, self.name)
+		method()
+		self.tearDown()
+		return result
+	def tearDown(self):
+		pass
+
+class TestResult:
+	def __init__(self):
+		self.runCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
+	def summary(self):
+		return "%d run, 0 failed" % self.runCount
+
+class WasRun(TestCase):
+	def __init__(self, name):
+		TestCase.__init__(self, name)
+	def setUp(self):
+		self.log = "setUp "
+	def testMethod(self):
+		self.log = self.log + "testMethod "
+	def testBrokenMethod(self):
+		raise Exception
+	def tearDown(self):
+		self.log = self.log + "tearDown "
+
+class TestCaseTest(TestCase):
+	def testTemplateMethod(self):
+		self.test= WasRun("testMethod")
+		self.test.run()
+		assert("setUp testMethod tearDown " == self.test.log)
+	def testResult(self):
+		self.test= WasRun("testMethod")
+		result = self.test.run()
+		assert("1 run, 0 failed" == result.summary())
+	def testFailedResult(self):
+		self.test= WasRun("testBrokenMethod")
+		result = self.test.run()
+		assert("1 run, 1 failed" == result.summary())
+
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
---
+++ ch22.py
@@ -0,0 +1,65 @@
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+	def setUp(self):
+		pass
+	def run(self):
+		result = TestResult()
+		result.testStarted()
+		self.setUp()
+		try:
+			method = getattr(self, self.name)
+			method()
+		except:
+			result.testFailed()
+		self.tearDown()
+		return result
+	def tearDown(self):
+		pass
+
+class TestResult:
+	def __init__(self):
+		self.runCount = 0
+		self.errorCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
+	def testFailed(self):
+		self.errorCount = self.errorCount + 1
+	def summary(self):
+		return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+class WasRun(TestCase):
+	def __init__(self, name):
+		TestCase.__init__(self, name)
+	def setUp(self):
+		self.log = "setUp "
+	def testMethod(self):
+		self.log = self.log + "testMethod "
+	def testBrokenMethod(self):
+		raise Exception
+	def tearDown(self):
+		self.log = self.log + "tearDown "
+
+class TestCaseTest(TestCase):
+	def testTemplateMethod(self):
+		self.test= WasRun("testMethod")
+		self.test.run()
+		assert("setUp testMethod tearDown " == self.test.log)
+	def testResult(self):
+		self.test= WasRun("testMethod")
+		result = self.test.run()
+		assert("1 run, 0 failed" == result.summary())
+	def testFailedResult(self):
+		self.test= WasRun("testBrokenMethod")
+		result = self.test.run()
+		assert("1 run, 1 failed" == result.summary())
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed" == result.summary())
+
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResultFormatting").run()
+TestCaseTest("testFailedResult").run()
